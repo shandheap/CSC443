13214a4114fcf3b13b33e656cf5673d4f5157b21
shandheap <shandheap@gmail.com>
2016-01-28 21:02:27 -0500

    Refactor max_ave_seq_disk and max_ave_rand_disk

--- a1/max_ave_rand_disk.cc
+++ a1/max_ave_rand_disk.cc
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 #include <map>
 
 typedef struct record {
@@ -29,54 +30,54 @@ int main(int argc, char *argv[]) {
 
     long block_size = atol(argv[1]);
     block_size += block_size % sizeof(Record);
-    int records_per_block = block_size / sizeof(Record);
-    Record * buffer = (Record *) calloc(records_per_block, sizeof(Record));
-
-    clock_t begin, end;
-    double time_elapsed, processing_rate;
-
+    
     /* Calculate the filesize */
     fseek(fp_read, 0L, SEEK_END);
     long filesize = ftell(fp_read);
-    fseek(fp_read, 0L, SEEK_SET);
     int num_records = filesize / sizeof(Record);
     block_size = (block_size > filesize) ? filesize : block_size;
-    int num_of_blocks = filesize / block_size;
+    int num_of_blocks = ceil(filesize / (double) block_size);
 
     /* Use map to track user follower count */
     map<int, int> users;
     int max_count = 0;
 
-    long rand_blocks[num_of_blocks];
+    /* Create array of random block indexes */
+    int rand_blocks[num_of_blocks];
     for (int i=0; i < num_of_blocks; i++)
         rand_blocks[i] = i;
-    
+    srand(time(0));
     random_shuffle(&rand_blocks[0], &rand_blocks[num_of_blocks-1]);
+
+
+    int records_per_block = block_size / sizeof(Record);
+    Record * buffer = (Record *) calloc(records_per_block, sizeof(Record));
+
+    int rec_rem = (filesize % block_size) / sizeof(Record);
+    int rec_count = records_per_block;
+
+    clock_t begin, end;
+    double time_elapsed, processing_rate;
 
     /* Read binary file from disk randomly */
     begin = clock();
     for (int i=0; i < num_of_blocks; i++) {
         // Set file pointer to random block
         fseek(fp_read, block_size * rand_blocks[i], SEEK_SET);
-        fread(buffer, sizeof(Record), records_per_block, fp_read);
-        for (int j=0; j < records_per_block; j++) {
-            int uid1 = buffer[j].uid1;
-            users[uid1]++;
-            if (max_count < users[uid1]) {
-                max_count = users[uid1];
-            }
+        
+        // Check if last block is not full, if so then read partial block
+        if (rand_blocks[i] == num_of_blocks-1 && rec_rem) {
+            rec_count = rec_rem;
+            fread(buffer, sizeof(Record), rec_count, fp_read);
         }
-    }
-    
-    // read the remainder of the file(last remaining incomplete block)
-    fseek(fp_read, block_size * num_of_blocks, SEEK_SET);
-    int rec_rem = num_records - num_of_blocks * records_per_block;
-    if (rec_rem > 0) {
-        fread(buffer, sizeof(Record), rec_rem, fp_read);
-        for (int j=0; j < rec_rem; j++) {
+        else {
+            rec_count = records_per_block;
+            fread(buffer, sizeof(Record), rec_count, fp_read);
+        }
+
+        for (int j=0; j < rec_count; j++) {
             int uid1 = buffer[j].uid1;
             users[uid1]++;
-            // keep track of the maximum
             if (max_count < users[uid1]) {
                 max_count = users[uid1];
             }
--- a1/max_ave_seq_disk.cc
+++ a1/max_ave_seq_disk.cc
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 #include <map>
 
 typedef struct record {
@@ -41,33 +42,31 @@ int main(int argc, char *argv[]) {
     fseek(fp_read, 0L, SEEK_SET);
     int num_records = filesize / sizeof(Record);
     block_size = (block_size > filesize) ? filesize : block_size;
-    int num_of_blocks = filesize / block_size;
+    int num_of_blocks = ceil(filesize / double(block_size));
 
     /* Use map to track user follower count */
     map<int, int> users;
     int max_count = 0;
 
+    int rec_rem = (filesize % block_size) / sizeof(Record);
+    int rec_count = records_per_block;
+
     /* Read binary file from disk */
     begin = clock();
     for (int i=0; i < num_of_blocks; i++) {
-        fread(buffer, sizeof(Record), records_per_block, fp_read);
-        for (int j=0; j < records_per_block; j++) {
-            int uid1 = buffer[j].uid1;
-            users[uid1]++;
-            if (max_count < users[uid1]) {
-                max_count = users[uid1];
-            }
+        // Check if last block is not full, if so then read partial block
+        if (i == num_of_blocks-1 && rec_rem) {
+            rec_count = rec_rem;
+            fread(buffer, sizeof(Record), rec_count, fp_read);
         }
-    }
-    
-    // read the remainder of the file(last remaining incomplete block)
-    int rec_rem = num_records - num_of_blocks * records_per_block;
-    if (rec_rem > 0) {
-        fread(buffer, sizeof(Record), rec_rem, fp_read);
-        for (int j=0; j < rec_rem; j++) {
+        else {
+            rec_count = records_per_block;
+            fread(buffer, sizeof(Record), rec_count, fp_read);
+        }
+
+        for (int j=0; j < rec_count; j++) {
             int uid1 = buffer[j].uid1;
             users[uid1]++;
-            // keep track of the maximum
             if (max_count < users[uid1]) {
                 max_count = users[uid1];
             }
@@ -79,7 +78,7 @@ int main(int argc, char *argv[]) {
     double ave_count = (double) num_records / (double) users.size();
     printf("Max follower count is %d.\n", max_count);
     printf("Average follower count is %f.\n", ave_count);
-
+ 
     /* Output processing rate */
     time_elapsed = (double) (end - begin) / CLOCKS_PER_SEC;
     processing_rate = (double) filesize / (time_elapsed * 1000000);
