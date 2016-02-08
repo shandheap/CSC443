4f0e902b45a3ba7dd8e779dbd2c899d866bbb46d
ashiqsilent <ashiq.silent@gmail.com>
2016-02-08 09:08:22 -0500

    remove the cache clearing from the bash scripts as we should not include them in our submission

--- a1/time_read_blocks.sh
+++ a1/time_read_blocks.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 > read_blocks_timing.txt
 
-sync
-sudo bash -c "echo 1 > /proc/sys/vm/drop_caches"
 ./max_ave_seq_disk 1024 >> read_blocks_timing.txt
 
-sync
-sudo bash -c "echo 1 > /proc/sys/vm/drop_caches"
 ./max_ave_seq_ram 1024 >> read_blocks_timing.txt
 
 python plot_read_blocks.py
--- a1/time_write_blocks.sh
+++ a1/time_write_blocks.sh
@@ -5,8 +5,6 @@
 
 for size in 512 1024 4096 8192 16384 32768 1048576 2097152 4194304
 do
-  sync
-  echo 3 > sudo /proc/sys/vm/drop_caches
   result=`./write_blocks g_plusAnonymized.csv $size`
   echo $result >> write_blocks_timing.txt
 done
