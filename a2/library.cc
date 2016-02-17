592deaa003ba4f7becc2982e557d180567d9e6e8
shandheap <shandheap@gmail.com>
2016-02-17 14:18:59 -0500

    Add qsort compare function

---
+++ a2/library.cc
@@ -0,0 +1,17 @@
+#include <library.h>
+
+
+/**
+ * * Compares two records a and b 
+ * * with respect to the value of the integer field f.
+ * * Returns an integer which indicates relative order: 
+ * * positive: record a > record b
+ * * negative: record a < record b
+ * * zero: equal records
+ * */
+int compare(const void *a, const void *b) {
+    int a_uid2 = ((const Record *) a)->uid2; 
+    int b_uid2 = ((const Record *) b)->uid2;
+
+    return a_uid2 - b_uid2;
+}
---
+++ a2/library.h
@@ -0,0 +1,6 @@
+typedef struct record {
+    int uid1;
+    int uid2;
+} Record;
+
+int compare(const void *a, const void *b);
